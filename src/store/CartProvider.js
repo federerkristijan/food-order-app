d665b1bf2447fa65fe31f640b88973680b42a5b7
Kristijan Federer <federer.kristijan@gmail.com>
2022-06-17 14:20:30 +0200

    removing cart items

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -4,46 +4,72 @@ import CartContext from "./cart-context";
 
 const defaultCartState = {
   items: [],
-  totalAmount: 0
+  totalAmount: 0,
 };
 
 const cartReducer = (state, action) => {
-  if (action.type === 'ADD_ITEM') {
+  if (action.type === "ADD_ITEM") {
     // concat add new item into an array and returns a new array (push updates an existing array)
-    const updatedItems = state.items.concat(action.item);
-    const updatedTotalAmount = state.totalAmount + action.item.price + action.item.amount;
+    const updatedTotalAmount =
+      state.totalAmount + action.item.price + action.item.amount;
+
+    // checking if the updated item already exist on the cart list
+    const existingCartItemIndex = state.items.findIndex(
+      (item) => item.id === action.item.id
+    );
+
+    // if the new item already exist on the list, it will be added on the existing item
+    const existingCartItem = state.items[existingCartItemIndex];
+    let updatedItems;
+
+    if (existingCartItem) {
+      const updatedItem = {
+        ...existingCartItem,
+        amount: existingCartItem.amount + action.item.amount
+      };
+      updatedItems = [...state.items];
+      // taking the old item in the array and overwritting it with a new one
+      updatedItems[existingCartItemIndex] = updatedItem
+    } else {
+      // brand new item
+      updatedItems = state.items.concat(action.item);
+    }
+
     return {
       items: updatedItems,
-      totalAmount: updatedTotalAmount
+      totalAmount: updatedTotalAmount,
     };
   }
   return defaultCartState;
 };
 
-const CartProvider = props => {
+const CartProvider = (props) => {
   // useReducer always needs to be inside a component!
-  const [cartState, dispatchCartAction] = useReducer(cartReducer, defaultCartState);
+  const [cartState, dispatchCartAction] = useReducer(
+    cartReducer,
+    defaultCartState
+  );
 
-const addItemToCartHandler = item => {
-  dispatchCartAction({type: 'ADD_ITEM', item: item});
-};
+  const addItemToCartHandler = (item) => {
+    dispatchCartAction({ type: "ADD_ITEM", item: item });
+  };
 
-const removeItemFromCartHandler = id => {
-  dispatchCartAction({type: 'REMOVE_ITEM', id: id})
-};
+  const removeItemFromCartHandler = (id) => {
+    dispatchCartAction({ type: "REMOVE_ITEM", id: id });
+  };
 
-const cartContext = {
-  items: cartState.items,
-  totalAmount: cartState.totalAmount,
-  addItem: addItemToCartHandler,
-  removeItem: removeItemFromCartHandler
-}
+  const cartContext = {
+    items: cartState.items,
+    totalAmount: cartState.totalAmount,
+    addItem: addItemToCartHandler,
+    removeItem: removeItemFromCartHandler,
+  };
 
   return (
     <CartContext.Provider value={cartContext}>
       {props.children}
     </CartContext.Provider>
-  )
+  );
 };
 
 export default CartProvider;
