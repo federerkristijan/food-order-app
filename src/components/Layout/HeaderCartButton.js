5e5dc3cbf4560caf512138648ccf666aed5bc8b4
Kristijan Federer <federer.kristijan@gmail.com>
2022-06-17 15:43:08 +0200

    food app done

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,19 +1,41 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 
 import CartContext from "../../store/cart-context";
 import CartIcon from "../Cart/CartIcon";
 import classes from "./HeaderCartButton.module.css";
 
 const HeaderCartButton = props => {
-   const cartCtx = useContext(CartContext);
+  const [btnIsBump, setBtnIsBump] = useState(false);
+  const cartCtx = useContext(CartContext);
+
+  const { items } = cartCtx;
 
   //  reduce method transforms an array of data into a single value
-   const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
+  const numberOfCartItems = items.reduce((curNumber, item) => {
     return curNumber + item.amount;
    }, 0);
 
+  const btnClasses = `${classes.button} ${btnIsBump ? classes.bump : ''}`;
+
+    useEffect(() => {
+      if(items.length === 0) {
+        return;
+      }
+      setBtnIsBump(true);
+
+      // reseting the animation
+      const timer = setTimeout(() => {
+        setBtnIsBump(false);
+      }, 300);
+
+      // with a cleanup function
+      return () => {
+        clearTimeout((timer));
+      };
+   }, [items]);
+
   return (
-    <button className={classes.button} onClick={props.onClick} >
+    <button className={btnClasses} onClick={props.onClick} >
       <span className={classes.icon}>
         <CartIcon />
       </span>
