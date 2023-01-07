4dbf32bee76f580fea524aa115b1aa1f0ff6e4bf
federerkristijan <federer.kristijan@gmail.com>
2023-01-07 11:52:19 +0100

    added .gitignore and loading component via useState

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,17 +1,18 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 
-import Card from '../UI/Card';
-import MealItem from './MealItem/MealItem';
-import classes from './AvailableMeals.module.css';
-
-
+import Card from "../UI/Card";
+import MealItem from "./MealItem/MealItem";
+import classes from "./AvailableMeals.module.css";
 
 const AvailableMeals = () => {
   const [meals, setMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    const fetchMeals = async() => {
-      const response = await fetch('https://udemy-react-21466-default-rtdb.europe-west1.firebasedatabase.app/meals.json');
+    const fetchMeals = async () => {
+      const response = await fetch(
+        "https://udemy-react-21466-default-rtdb.europe-west1.firebasedatabase.app/meals.json"
+      );
       const responseData = await response.json();
 
       const loadedMeals = [];
@@ -21,15 +22,24 @@ const AvailableMeals = () => {
           id: key,
           name: responseData[key].name,
           description: responseData[key].description,
-          price: responseData[key].price
-        })
+          price: responseData[key].price,
+        });
       }
 
       setMeals(loadedMeals);
+      setIsLoading(false);
     };
 
     fetchMeals();
-  }, [])
+  }, []);
+
+  if (isLoading) {
+    return (
+      <section className={classes.MealsLoading}>
+        <p>Loading...</p>
+      </section>
+    );
+  }
 
   const mealsList = meals.map((meal) => (
     <MealItem
