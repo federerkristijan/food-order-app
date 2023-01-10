1f51c3b11b0a2ee84927c1edde2964ba28007527
federerkristijan <federer.kristijan@gmail.com>
2023-01-10 10:44:30 +0100

    checkout.js - useRef for all 4 inputs

--- src/components/Cart/Checkout.js
+++ src/components/Cart/Checkout.js
@@ -1,6 +1,13 @@
+import { useRef } from "react";
+
 import classes from "./Checkout.module.css";
 
 const Checkout = (props) => {
+  const nameInputRef = useRef();
+  const streetInputRef = useRef();
+  const postalInputRef = useRef();
+  const cityInputRef = useRef();
+
   const confirmHandler = (event) => {
     event.preventdefault();
   };
@@ -9,19 +16,19 @@ const Checkout = (props) => {
     <form className={classes.form} onSubmit={confirmHandler}>
       <div className={classes.control}>
         <label htmlFor="name">Your name</label>
-        <input type="text" id="name" />
+        <input type="text" id="name" ref={nameInputRef} />
       </div>
       <div className={classes.control}>
         <label htmlFor="street">Your street</label>
-        <input type="text" id="street" />
+        <input type="text" id="street" ref={streetInputRef} />
       </div>
       <div className={classes.control}>
         <label htmlFor="postal">Postal code</label>
-        <input type="text" id="postal" />
+        <input type="text" id="postal" ref={postalInputRef} />
       </div>
       <div className={classes.control}>
         <label htmlFor="city">Your city</label>
-        <input type="text" id="city" />
+        <input type="text" id="city" ref={cityInputRef} />
       </div>
       <div className={classes.action}>
         {/* type="button doesn't submit the form!" */}
