7306e29c5ec6aef19d9bd327bc0ffcd989b62f04
federerkristijan <federer.kristijan@gmail.com>
2023-01-13 12:25:30 +0100

    ibid. added to CartContext

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -86,6 +86,7 @@ const CartProvider = (props) => {
     totalAmount: cartState.totalAmount,
     addItem: addItemToCartHandler,
     removeItem: removeItemFromCartHandler,
+    clearCart: clearCartHandler
   };
 
   return (
